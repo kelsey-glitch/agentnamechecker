431b03a9b87a11f4bb340f76c52fb72fb0ed1631
root <root@srv1419385.hstgr.cloud>
2026-02-25 00:08:28 +0000

    🔧 Fix base path for agentnamechecker repo

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/VIPAIAGENTNAMECHECKERGAME/',
+  base: '/agentnamechecker/',
   build: {
     outDir: 'dist',
   },
